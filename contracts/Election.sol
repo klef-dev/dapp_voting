d3de7bceec91a7f568de3ccecb1d77d8297b6dee
Abraham Ugbeshe <abrahamugbeshe@gmail.com>
2021-01-20 18:52:20 +0100

    ⚡️ Added candidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,9 +2,28 @@
 pragma solidity >=0.4.22 <0.8.0;
 
 contract Election {
-    string public candidate;
+    // Model a Candidate
+    struct Candidate {
+        uint256 id;
+        string name;
+        uint256 voteCount;
+    }
+
+    // Store Candidates
+    mapping(uint256 => Candidate) public candidates;
+
+    // Fetch Candidate
+
+    // Store Candidates Count
+    uint256 public candidatesCount;
 
     constructor() public {
-        candidate = "Buhari";
+        addCandidate("Buhari");
+        addCandidate("Atiku");
+    }
+
+    function addCandidate(string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
