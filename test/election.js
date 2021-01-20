fa362f32795d2042bc0a432defb8e18a3858ec3c
Abraham Ugbeshe <abrahamugbeshe@gmail.com>
2021-01-20 19:04:01 +0100

    ✅ Wrote test to check if it was initialized with two candidates

---
+++ test/election.js
@@ -0,0 +1,11 @@
+let Election = artifacts.require("./Election.sol")
+
+contract("Election", (accounts) => {
+  it("initializes with two candidates", () => {
+    return Election.deployed().then((instance) => {
+      return instance.candidatesCount();
+    }).then((count) => {
+      assert.equal(count, 2)
+    })
+  })
+})
