779b85b191f4db220124d5765fe32fc086b1eee9
Abraham Ugbeshe <abrahamugbeshe@gmail.com>
2021-01-20 19:56:55 +0100

    ⚡️ Added voting functionality

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -9,6 +9,9 @@ contract Election {
         uint256 voteCount;
     }
 
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+
     // Store Candidates
     mapping(uint256 => Candidate) public candidates;
 
@@ -16,6 +19,9 @@ contract Election {
 
     // Store Candidates Count
     uint256 public candidatesCount;
+
+    // Voted event
+    event votedEvent(uint256 indexed _candidateId);
 
     constructor() public {
         addCandidate("Buhari");
@@ -25,5 +31,22 @@ contract Election {
     function addCandidate(string memory _name) private {
         candidatesCount++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint256 _candidateId) public {
+        // Require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // Require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // Record that voter has voted
+        voters[msg.sender] = true;
+
+        // Update candidate vote Count
+        candidates[_candidateId].voteCount++;
+
+        // Trigger voted event
+        emit votedEvent(_candidateId);
     }
 }
